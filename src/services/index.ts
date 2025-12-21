d68ea657f6e5220545cb0f21ae7541de59325536
Omri Hiba <hibaomri10@gmail.com>
2025-12-21 19:53:12 +0100

    Add index.ts

---
+++ src/services/index.ts
@@ -0,0 +1,8 @@
+export { default as db } from './db';
+export * from './db';
+
+export { default as AuthService } from './AuthService';
+export * from './AuthService';
+
+export { default as RecommendationService } from './RecommendationService';
+export * from './RecommendationService';

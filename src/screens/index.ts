31bf05474516fb1ce98df7c681ab710c205a9ef3
Abdelkader elassali <abdoelassali66@gmail.com>
2025-12-28 13:15:49 +0100

    Add screen exports

---
+++ src/screens/index.ts
@@ -0,0 +1,7 @@
+export { default as MovieDetailScreen } from './MovieDetailScreen';
+export { default as ForYouScreen } from './ForYouScreen';
+export { default as SearchScreen } from './SearchScreen';
+export { default as CollectionsScreen } from './CollectionsScreen';
+export { default as ProfileScreen } from './ProfileScreen';
+export { default as LoginScreen } from './LoginScreen';
+export { default as RegisterScreen } from './RegisterScreen';
